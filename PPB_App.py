47cd3c19ffda2b909cd902a787f899876bf2d053
FrancoisGibault <95180928+FrancoisGibault@users.noreply.github.com>
2023-05-22 13:16:57 +0200

    Update PPB_App.py

--- PPB_App.py
+++ PPB_App.py
@@ -249,7 +249,8 @@ def stats():
     ax.spines[:].set_color(COLOR)
     ax.patch.set_facecolor('black')
     plt.grid(axis = 'y', color='dimgrey')
-    sns.barplot(data =dm, x='Mois', y="Comptage horaire", hue='Annee', color = ['royalblue', 'chocolate'])
+    sns.color_palette("Paired")
+    sns.barplot(data =dm, x='Mois', y="Comptage horaire", hue='Annee')
     plt.legend(frameon=False);
     plt.style.use("dark_background")
     st.pyplot(fig)
